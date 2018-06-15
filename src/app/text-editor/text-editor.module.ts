c94b2bdaf5c7cfad3fbb338d63e228db84cbb492
Julien Landry <julien.r.lan@googlemail.com>
2018-06-15 11:07:06 -0400

    feat(#146): Add necessary module imports

--- src/app/text-editor/text-editor.module.ts
+++ src/app/text-editor/text-editor.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TextEditorComponent } from './text-editor.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CKEditorModule } from 'ng2-ckeditor';
+import { LoadingModule } from 'ngx-loading';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    CKEditorModule,
+    LoadingModule,
+    HttpClientModule,
+    FormsModule
   ],
   exports: [
     TextEditorComponent
